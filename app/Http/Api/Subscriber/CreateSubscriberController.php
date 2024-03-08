6c56ab0968fff28de17a128fe462add87a97d8b9
MuhammadNoor <noor4apps@gmail.com>
2024-03-08 21:59:14 +0300

    The Real Power of DTOs and Actions: Subscriber API

---
+++ app/Http/Api/Subscriber/CreateSubscriberController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Api\Subscriber;
+
+use Domain\Subscriber\Actions\UpsertSubscriberAction;
+use Domain\Subscriber\DataTransferObjects\SubscriberData;
+use Illuminate\Http\Request;
+
+class CreateSubscriberController
+{
+    public function __invoke(SubscriberData $data, Request $request): SubscriberData
+    {
+        $subscriber = UpsertSubscriberAction::execute($data, $request->user());
+
+        return $subscriber->getData();
+    }
+}
