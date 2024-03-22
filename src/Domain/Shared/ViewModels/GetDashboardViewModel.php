c898a42f5e7ff6d441134771806df1f00769a344
MuhammadNoor <noor4apps@gmail.com>
2024-03-22 18:07:24 +0300

    Dashboard And Reports: All-Time Performance

--- src/Domain/Shared/ViewModels/GetDashboardViewModel.php
+++ src/Domain/Shared/ViewModels/GetDashboardViewModel.php
@@ -2,7 +2,10 @@
 
 namespace Domain\Shared\ViewModels;
 
+use Domain\Mail\DataTransferObjects\PerformanceData;
+use Domain\Mail\Models\SentMail;
 use Domain\Shared\Models\User;
+use Domain\Shared\ValueObjects\Percent;
 use Domain\Subscriber\DataTransferObjects\NewSubscribersCountData;
 use Domain\Shared\Filters\DateFilter;
 use Domain\Subscriber\Models\Subscriber;
@@ -20,6 +23,31 @@ class GetDashboardViewModel extends ViewModel
             this_month: Subscriber::whereSubscribedBetween(DateFilter::thisMonth())->count(),
             this_week: Subscriber::whereSubscribedBetween(DateFilter::thisWeek())->count(),
             today: Subscriber::whereSubscribedBetween(DateFilter::today())->count(),
+        );
+    }
+
+    public function performance(): PerformanceData
+    {
+        $total = SentMail::count();
+
+        return new PerformanceData(
+            total: $total,
+            open_rate: $this->averageOpenRate($total),
+            click_rate: $this->averageClickRate($total),
+        );
+    }
+
+    private function averageOpenRate(int $total): Percent
+    {
+        return Percent::from(
+            SentMail::whereOpened()->count(), $total
+        );
+    }
+
+    private function averageClickRate(int $total): Percent
+    {
+        return Percent::from(
+            SentMail::whereClicked()->count(), $total
         );
     }
 
